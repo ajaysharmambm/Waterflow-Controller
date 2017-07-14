dfb931d6b98b0036e1e83d4933172a28df3a2129
ajaysharmambm <ajaybassi2011@gmail.com>
2017-07-14 19:01:49 +0530

    Final Code

---
+++ Flow_Controller.ino
@@ -0,0 +1,23 @@
+int ledPin = 13;
+int inPin = 2;
+static int counter;
+int ppl = 450;
+
+void setup(){
+  pinMode(inPin, INPUT);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(inPin), pulseCounter, RISING);
+}
+
+void loop(){
+  Serial.println(counter);
+  if(counter>=450*3){
+    noInterrupts();
+    digitalWrite(13, HIGH);
+  }
+}
+
+void pulseCounter(){
+  counter++;
+}
+
